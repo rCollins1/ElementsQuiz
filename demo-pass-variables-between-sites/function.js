0c3500d546f326fa825c36a0eb7a018db98b5ea4
Yining Wang <kriswang5747@gmail.com>
2017-11-23 17:23:49 -0500

    remove extra test code

--- demo-pass-variables-between-sites/function.js
+++ demo-pass-variables-between-sites/function.js
@@ -3,7 +3,6 @@ var indicator = 2;
 function openNewpage (){
     var title = document.getElementById('txt-name').value;
 
-    title = '&#10003;'
     if (indicator === 1) {
     	window.open("../report-page-conservative/report-conservative.html?form/title=" + title);
     }
